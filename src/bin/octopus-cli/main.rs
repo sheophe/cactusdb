ed80037d3384142695d95db0f081815311ce24bb
thelyx <pvakhnivskyi@telecard.kiev.ua>
2023-01-28 22:29:00 +0200

    .gitignore cleanup

---
+++ src/bin/octopus-cli/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("octopusmq CLI started");
+    println!("dead af!");
+}
--- src/client/main.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    println!("octopusmq client started");
-    println!("dead af!");
-}
